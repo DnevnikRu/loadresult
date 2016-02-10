d996e2ac6820f6a529a6e400c94386a8fdf9c94d
Евгений Морозов <morozov@company.dnevnik.ru>
2016-02-10 18:50:38 +0300

    DA-602 Add a template to the upload results page

--- config/routes.rb
+++ config/routes.rb
@@ -53,4 +53,6 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+
+  resources :results, only: [:index, :new, :create]
 end

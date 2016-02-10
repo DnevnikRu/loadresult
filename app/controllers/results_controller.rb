c1e22832db3af7fefb581fd81f3e536506902be6
Evgeny Morozov <evmorov@gmail.com>
2016-02-11 02:21:08 +0300

    DA-605 Add the first version of the upload form

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -1,4 +1,19 @@
 class ResultsController < ApplicationController
+  def index
+  end
+
   def new
+  end
+
+  def create
+    version = params[:version]
+    rps = params[:rps]
+    duration = params[:duration]
+    profile = params[:profile]
+    date = params[:date]
+    reqests_data = params[:reqests_data]
+    perfmon_data = params[:perfmon_data]
+
+    render action: 'index'
   end
 end

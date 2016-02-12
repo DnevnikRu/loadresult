4e9a0a0b094d635ebb9b308f89f07fd30c968210
Алексей Москвин <moskvin@company.dnevnik.ru>
2016-02-12 19:15:14 +0300

    DA-606 fix test after rename PerformanceResult model

--- spec/factories/performance_result.rb
+++ spec/factories/performance_result.rb
@@ -1,5 +1,5 @@
 FactoryGirl.define do
-  factory :perfomance_result do
+  factory :performance_result do
     timestamp 1455023039547
     label 'EXEC Network\Bytes Sent/sec'
     value '1784387'
--- spec/models/perfomance_result_result_spec.rb
+++ spec/models/perfomance_result_result_spec.rb
@@ -4,7 +4,7 @@ describe PerformanceResult do
 
   it 'belong to results' do
     result = create(:result, version: 'test')
-    perfomance_result = create(:perfomance_result, result_id: result.id)
+    perfomance_result = create(:performance_result, result_id: result.id)
     expect(perfomance_result.result.version).to eql('test')
   end
 

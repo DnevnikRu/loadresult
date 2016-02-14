7fa6e2e1200ca57ef102cb10bae5d32b50bce1b8
Evgeny Morozov <evmorov@gmail.com>
2016-02-14 18:11:26 +0300

    Add a test

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -7,12 +7,6 @@ class ResultsController < ApplicationController
   end
 
   def create
-    if rand(1..2) == 1
-      redirect_to({action: :index}, notice: 'Result was successfully created.')
-    else # error
-      @errors = ['first error', 'second error', 'third error']
-      redirect_to({action: :new}, alert: @errors)
-    end
+    redirect_to({action: :index}, notice: 'Result was successfully created.')
   end
-
 end
--- spec/features/upload_result_spec.rb
+++ spec/features/upload_result_spec.rb
@@ -17,7 +17,18 @@ feature 'Upload load result' do
     expect(page).to have_css('h1', text: 'Upload results')
   end
 
-  scenario 'Upload a result with all fields filled'
+  scenario 'Upload a result with all fields filled' do
+    visit '/results/new'
+    fill_in 'version', with: 'edu sharding'
+    fill_in 'rps', with: '600'
+    fill_in 'duration', with: '600'
+    fill_in 'profile', with: 'all_sites'
+    fill_in 'date_', with: '14.02.2016 17:45'
+    attach_file 'requests_data', File.expand_path('../../resources/summary.csv', __FILE__)
+    attach_file 'perfmon_data', File.expand_path('../../resources/perfmon.csv', __FILE__)
+    click_button 'Upload'
+    expect(page).to have_content('Result was successfully created.')
+  end
 
   scenario 'Upload a result without a perfmon file'
 

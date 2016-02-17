54eb3d9c2e6035d5c39729494a14849251173ecb
Evgeny Morozov <evmorov@gmail.com>
2016-02-18 01:42:52 +0300

    DA-611 Move to ajax

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -9,9 +9,9 @@ class ResultsController < ApplicationController
   def create
     result = Result.upload_and_create(params)
     if result.errors.empty?
-      redirect_to({action: :index}, notice: 'Result was successfully created.')
+      redirect_to({ action: :index }, notice: 'Result was successfully created.')
     else
-      redirect_to({action: :new}, alert: result.errors.full_messages)
+      redirect_to({ action: :new }, alert: result.errors.full_messages)
     end
   end
 
@@ -19,80 +19,60 @@ class ResultsController < ApplicationController
   end
 
   def request_chart
-    @data = {# mock data
-             current: {
-                 web00: {
-                     x: [1, 2, 3, 4],
-                     y: [5, 10, 11, 14]
-                 },
-                 web01: {
-                     x: [1, 2, 3, 4],
-                     y: [14, 3, 7, 8]
-                 }
-             },
-             base: {
-                 web00: {
-                     x: [1, 2, 3, 4],
-                     y: [10, 15, 13, 17]
-                 },
-                 web01: {
-                     x: [1, 2, 3, 4],
-                     y: [16, 5, 11, 9]
-                 }
-             }
-    }
-    @name = 'Requests current'
+    @data = params[:data]
+    @name = params[:name]
+    @div_id = params[:div_id]
     respond_to do |format|
       format.js
     end
   end
 
   def compare_chart_temp
-    @data = {# mock data
-             requests1: {
-                 current: {
-                     web00: {
-                         x: [1, 2, 3, 4],
-                         y: [5, 10, 11, 14]
-                     },
-                     web01: {
-                         x: [1, 2, 3, 4],
-                         y: [14, 3, 7, 8]
-                     }
-                 },
-                 base: {
-                     web00: {
-                         x: [1, 2, 3, 4],
-                         y: [10, 15, 13, 17]
-                     },
-                     web01: {
-                         x: [1, 2, 3, 4],
-                         y: [16, 5, 11, 9]
-                     }
-                 }
-             },
-             requests2: {
-                 current: {
-                     web00: {
-                         x: [1, 2, 3, 4],
-                         y: [4, 2, 10, 8]
-                     },
-                     web01: {
-                         x: [1, 2, 3, 4],
-                         y: [10, 9, 0, 4]
-                     }
-                 },
-                 base: {
-                     web00: {
-                         x: [1, 2, 3, 4],
-                         y: [57, 10, 11, 13]
-                     },
-                     web01: {
-                         x: [1, 2, 3, 4],
-                         y: [10, 12, 11, 10]
-                     }
-                 }
-             }
+    @results = { # mock data
+      request_chart1: {
+        current: {
+          web00: {
+            x: [1, 2, 3, 4],
+            y: [5, 10, 11, 14]
+          },
+          web01: {
+            x: [1, 2, 3, 4],
+            y: [14, 3, 7, 8]
+          }
+        },
+        base: {
+          web00: {
+            x: [1, 2, 3, 4],
+            y: [10, 15, 13, 17]
+          },
+          web01: {
+            x: [1, 2, 3, 4],
+            y: [16, 5, 11, 9]
+          }
+        }
+      },
+      request_chart2: {
+        current: {
+          web00: {
+            x: [1, 2, 3, 4],
+            y: [23, 34, 35, 34]
+          },
+          web01: {
+            x: [1, 2, 3, 4],
+            y: [23, 26, 16, 10]
+          }
+        },
+        base: {
+          web00: {
+            x: [1, 2, 3, 4],
+            y: [10, 16, 13, 17]
+          },
+          web01: {
+            x: [1, 2, 3, 4],
+            y: [16, 45, 13, 9]
+          }
+        }
+      }
     }
   end
 end

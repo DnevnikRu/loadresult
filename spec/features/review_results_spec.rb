0aaa37bbafa685002d6360c4cf501527c3c31ff5
Марта Суббота <subbota@company.dnevnik.ru>
2016-02-17 11:08:20 +0300

    DA-604 fix table id and add range

--- spec/features/review_results_spec.rb
+++ spec/features/review_results_spec.rb
@@ -4,18 +4,20 @@ require 'active_support/time_with_zone'
 feature 'Review results' do
   scenario 'Uploaded results exist on the results page' do
     result = create(:result)
+    actual_create_at =Time.zone.parse(result.created_at.to_s)
+    time_range = actual_create_at-60..actual_create_at+60
     expected_result =
-      [result.version, result.rps.to_s, result.duration.to_s, result.profile, Time.zone.parse(result.test_run_date.to_s).strftime('%d.%m.%Y %H:%M'), Time.zone.parse(result.created_at.to_s).strftime('%d.%m.%Y %H:%M')]
+        [result.version, result.rps.to_s, result.duration.to_s, result.profile, Time.zone.parse(result.test_run_date.to_s).strftime('%d.%m.%Y %H:%M'), time_range.cover?(actual_create_at)]
     visit '/results/'
     results_rows = []
-    page.all('table#results tr.result_row').each do |row|
+    page.all('table#results-table tr.result_row').each do |row|
       version = row.find('td.version').text
       rps = row.find('td.rps').text
       duration = row.find('td.duration').text
       profile = row.find('td.profile').text
       run_date = Time.zone.parse(row.find('td.test_run_date').text).strftime('%d.%m.%Y %H:%M')
-      created_at = Time.zone.parse(row.find('td.created_at').text).strftime('%d.%m.%Y %H:%M')
-      results_rows.push [version, rps, duration, profile, run_date, created_at]
+      created_at =Time.zone.parse(row.find('td.created_at').text)
+      results_rows.push [version, rps, duration, profile, run_date, time_range.cover?(created_at)]
     end
     expect(results_rows.include?(expected_result)).to be(true), 'Result from database is not displayed'
   end
@@ -34,18 +36,19 @@ feature 'Review results' do
       attach_file 'requests_data', summary_file_path
       attach_file 'perfmon_data', perfmon_file_path
       click_button 'Upload'
-      actual_create_at = Time.now.utc.strftime('%d.%m.%Y %H:%M')
-      expected_row = ['edu sharding', '600', '600', 'all_sites', '14.02.2016 17:45', actual_create_at]
+      actual_create_at = Time.now.utc
+      time_range = actual_create_at-60..actual_create_at+60
+      expected_row = ['edu sharding', '600', '600', 'all_sites', '14.02.2016 17:45', time_range.cover?(actual_create_at)]
       results_rows = []
-      page.all('table#results tr.result_row').each do |row|
+      page.all('table#results-table tr.result_row').each do |row|
         version = row.find('td.version').text
         rps = row.find('td.rps').text
         duration = row.find('td.duration').text
         profile = row.find('td.profile').text
         run_date = Time.parse(row.find('td.test_run_date').text)
         run_date = run_date.strftime('%d.%m.%Y %H:%M')
-        created_at = Time.parse(row.find('td.created_at').text).strftime('%d.%m.%Y %H:%M')
-        results_rows.push [version, rps, duration, profile, run_date, created_at]
+        created_at =Time.parse(row.find('td.created_at').text)
+        results_rows.push [version, rps, duration, profile, run_date, time_range.cover?(created_at)]
       end
       expect(results_rows.include?(expected_row)).to be(true), 'Just uploaded result is not displayed'
     end

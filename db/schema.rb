49ee28c92460c629b0e689d3e9ec5e3eb92d63d7
Evgeny Morozov <evmorov@gmail.com>
2016-02-19 00:22:07 +0300

    Move back psql extension

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20160217154121) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "performance_results", force: :cascade do |t|
     t.integer  "result_id"
     t.integer  "timestamp",  limit: 8

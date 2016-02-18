52b7c5ba4a60a91276653341b16bceaec29894f9
Евгений Морозов <morozov@company.dnevnik.ru>
2016-02-18 11:54:16 +0300

    DA-611 Find data for a chart in controller instead of sending in request

--- app/controllers/compare_controller.rb
+++ app/controllers/compare_controller.rb
@@ -3,8 +3,8 @@ class CompareController < ApplicationController
   end
 
   def request_chart
-    @data = params[:data]
-    @name = params[:name]
+    @data = mock_data[params[:data_name].to_sym]
+    @chart_name = params[:chart_name]
     @div_id = params[:div_id]
     respond_to do |format|
       format.js
@@ -12,51 +12,55 @@ class CompareController < ApplicationController
   end
 
   def chart_example
-    @results = { # mock data
-      request_chart1: {
-        current: {
-          web00: {
-            x: [1, 2, 3, 4],
-            y: [5, 10, 11, 14]
-          },
-          web01: {
-            x: [1, 2, 3, 4],
-            y: [14, 3, 7, 8]
-          }
+    @data = mock_data
+  end
+
+  def mock_data
+    {
+        request_chart1: {
+            current: {
+                web00: {
+                    x: [1, 2, 3, 4],
+                    y: [5, 10, 11, 14]
+                },
+                web01: {
+                    x: [1, 2, 3, 4],
+                    y: [14, 3, 7, 8]
+                }
+            },
+            base: {
+                web00: {
+                    x: [1, 2, 3, 4],
+                    y: [10, 15, 13, 17]
+                },
+                web01: {
+                    x: [1, 2, 3, 4],
+                    y: [16, 5, 11, 9]
+                }
+            }
         },
-        base: {
-          web00: {
-            x: [1, 2, 3, 4],
-            y: [10, 15, 13, 17]
-          },
-          web01: {
-            x: [1, 2, 3, 4],
-            y: [16, 5, 11, 9]
-          }
+        request_chart2: {
+            current: {
+                web00: {
+                    x: [1, 2, 3, 4],
+                    y: [23, 34, 35, 34]
+                },
+                web01: {
+                    x: [1, 2, 3, 4],
+                    y: [23, 26, 16, 10]
+                }
+            },
+            base: {
+                web00: {
+                    x: [1, 2, 3, 4],
+                    y: [10, 16, 13, 17]
+                },
+                web01: {
+                    x: [1, 2, 3, 4],
+                    y: [16, 45, 13, 9]
+                }
+            }
         }
-      },
-      request_chart2: {
-        current: {
-          web00: {
-            x: [1, 2, 3, 4],
-            y: [23, 34, 35, 34]
-          },
-          web01: {
-            x: [1, 2, 3, 4],
-            y: [23, 26, 16, 10]
-          }
-        },
-        base: {
-          web00: {
-            x: [1, 2, 3, 4],
-            y: [10, 16, 13, 17]
-          },
-          web01: {
-            x: [1, 2, 3, 4],
-            y: [16, 45, 13, 9]
-          }
-        }
-      }
     }
   end
 end

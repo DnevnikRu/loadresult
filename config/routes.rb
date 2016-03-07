e563c7206045de9534a463ba7d02b7e82b36f71c
Evgeny Morozov <evmorov@gmail.com>
2016-03-07 19:00:43 +0300

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
-  resources :results, only: [:index, :new, :create]
-  get 'compare' => 'compare#show'
-  post 'compare/requests_histogram_plot' => 'compare#requests_histogram_plot'
-  post 'compare/all_requests_histogram_plot' => 'compare#all_requests_histogram_plot'
-  post 'compare/percentile_requests_plot' => 'compare#percentile_requests_plot'
   root 'results#index'
+  resources :results, only: [:index, :new, :create]
+
+  get 'compare' => 'compare#show'
+  namespace 'compare' do
+    post 'requests_histogram_plot'
+    post 'all_requests_histogram_plot'
+    post 'percentile_requests_plot'
+    post 'performance_plot'
+  end
 end

67f2a4dd1f126392c3e1d415658618435ccdbd8a
Евгений Морозов <morozov@company.dnevnik.ru>
2016-03-10 16:10:54 +0300

    DA-632 Редактирование загруженного результата

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -1,5 +1,5 @@
 class ResultsController < ApplicationController
-  before_action :set_result, only: [:destroy]
+  before_action :set_result, only: [:edit, :update, :destroy]
 
   def index
     @results = Result.all
@@ -8,10 +8,35 @@ class ResultsController < ApplicationController
   def new
   end
 
+  def edit
+    flash[:version] = @result[:version]
+    flash[:rps] = @result[:rps]
+    flash[:duration] = @result[:duration]
+    flash[:profile] = @result[:profile]
+  end
+
+  def update
+    update_result = @result.update(
+        version: params[:version],
+        rps: params[:rps],
+        duration: params[:duration],
+        profile: params[:profile]
+    )
+    if update_result
+      redirect_to(results_url, notice: 'Result was successfully updated.')
+    else
+      flash[:version] = params[:version]
+      flash[:rps] = params[:rps]
+      flash[:duration] = params[:duration]
+      flash[:profile] = params[:profile]
+      redirect_to({ action: :edit }, alert: @result.errors.full_messages)
+    end
+  end
+
   def create
     result = Result.upload_and_create(params)
     if result.errors.empty?
-      redirect_to({ action: :index }, notice: 'Result was successfully created.')
+      redirect_to(results_url, notice: 'Result was successfully created.')
     else
       flash[:version] = result[:version]
       flash[:rps] = result[:rps]

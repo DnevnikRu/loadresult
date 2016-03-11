91a8b829de4699545c7ce9cf2b6fce5b71142d61
Евгений Морозов <morozov@company.dnevnik.ru>
2016-03-11 16:32:04 +0300

    DA-629 Move method that connected with data for plots from controller to model

--- app/controllers/compare_controller.rb
+++ app/controllers/compare_controller.rb
@@ -13,78 +13,28 @@ class CompareController < ApplicationController
 
   def requests_histogram_plot
     @plot_id = params[:plot_id]
-    @result1_data = data_for_requests_histogram_plot(params[:result1_id], params[:label])
-    @result2_data = data_for_requests_histogram_plot(params[:result2_id], params[:label])
-  end
-
-  def data_for_requests_histogram_plot(result_id, label)
-    bottom_timestamp, top_timestamp = Result.border_timestamps(result_id, RequestsResult)
-    records = RequestsResult.where(Result.where_conditional(result_id, label, bottom_timestamp, top_timestamp))
-    records.map(&:value)
+    @result1_data = Result.requests_histogram_plot(params[:result1_id], params[:label])
+    @result2_data = Result.requests_histogram_plot(params[:result2_id], params[:label])
   end
 
   def all_requests_histogram_plot
-    @result1_data = data_for_all_requests_histogram_plot(params[:result1_id])
-    @result2_data = data_for_all_requests_histogram_plot(params[:result2_id])
-  end
-
-  def data_for_all_requests_histogram_plot(result_id)
-    bottom_timestamp, top_timestamp = Result.border_timestamps(result_id, RequestsResult)
-    records = RequestsResult.where(Result.where_conditional(result_id, nil, bottom_timestamp, top_timestamp))
-    records.map(&:value)
+    @result1_data = Result.all_requests_histogram_plot(params[:result1_id])
+    @result2_data = Result.all_requests_histogram_plot(params[:result2_id])
   end
 
   def percentile_requests_plot
-    @result1_data = data_for_percentile_requests_plot(params[:result1_id])
-    @result2_data = data_for_percentile_requests_plot(params[:result2_id])
-  end
-
-  def data_for_percentile_requests_plot(result_id)
-    bottom_timestamp, top_timestamp = Result.border_timestamps(result_id, RequestsResult)
-    records = RequestsResult.where(Result.where_conditional(result_id, nil, bottom_timestamp, top_timestamp))
-    values = records.map(&:value)
-    (0..100).map { |i| Result.percentile(values, i) }
+    @result1_data = Result.percentile_requests_plot(params[:result1_id])
+    @result2_data = Result.percentile_requests_plot(params[:result2_id])
   end
 
   def performance_plot
     @group_name = params[:group_name]
-    performance_group = find_performance_group(params[:group_name])
+    performance_group = PerformanceGroup.find_by(name: params[:group_name])
     @unit = performance_group.units
     @plot_id = params[:plot_id]
     @result1_id = params[:result1_id]
     @result2_id = params[:result2_id]
-    @result1_data = data_for_performance_plot(params[:result1_id], performance_group)
-    @result2_data = data_for_performance_plot(params[:result2_id], performance_group)
-  end
-
-  def data_for_performance_plot(result_id, performance_group)
-    data = {}
-    performance_group.labels.each do |label_main|
-      label_main_label = handle_backslash(label_main.label)
-      labels = find_performance_result_labels(label_main_label).uniq
-      labels.each do |label|
-        data[label] = { seconds: [], values: [] }
-        bottom_timestamp, top_timestamp = Result.border_timestamps(result_id, PerformanceResult)
-        records = PerformanceResult.where(Result.where_conditional(result_id, label, bottom_timestamp, top_timestamp))
-        timestamp_min = records.minimum(:timestamp)
-        records.order(:timestamp).each do |record|
-          data[label][:seconds].push (record.timestamp - timestamp_min) / 1000
-          data[label][:values].push record.value
-        end
-      end
-    end
-    data
-  end
-
-  def find_performance_group(group_name)
-    PerformanceGroup.find_by(name: group_name)
-  end
-
-  def handle_backslash(str)
-    str.gsub('\\') { '\\\\' }
-  end
-
-  def find_performance_result_labels(label)
-    PerformanceResult.where('label LIKE ?', "%#{label}%").pluck(:label)
+    @result1_data = Result.performance_plot(params[:result1_id], performance_group)
+    @result2_data = Result.performance_plot(params[:result2_id], performance_group)
   end
 end

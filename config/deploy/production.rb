dca77eca6fbde78af35d1ca7133612635e1ee0f8
Алексей Москвин <moskvin@company.dnevnik.ru>
2016-03-16 18:53:08 +0300

    DA-625 run db:seed on production

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -8,3 +8,7 @@ server 'loadresult', user: 'deployer', roles: %w{web app}
 set :deploy_to, '/var/www/loadresult'
 set :rails_env, 'production'
 set :bundle_without, nil
+
+namespace :deploy do
+  after 'deploy:migrate', :db_seed
+end
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -10,18 +10,5 @@ set :rails_env, 'test'
 set :bundle_without, nil
 
 namespace :deploy do
-
-  desc 'Run models tests'
-  task :run_model_tests => [:set_rails_env] do
-    on roles(:app) do
-      within release_path do
-        with rails_env: fetch(:rails_env) do
-          execute :rake, 'spec:models'
-        end
-      end
-    end
-  end
-
   after 'deploy:migrate', :db_seed
-
 end

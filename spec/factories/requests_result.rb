fcb4b951933d2a89c985f80aec951cc2d07bc8f4
Евгений Морозов <morozov@company.dnevnik.ru>
2016-03-28 15:58:37 +0300

    Remove extra space

--- spec/factories/requests_result.rb
+++ spec/factories/requests_result.rb
@@ -2,7 +2,7 @@ FactoryGirl.define do
   factory :requests_result do
     timestamp 1455023039547
     label 'children /marks.aspx:GET:tab=subject'
-    value  123
+    value 123
     response_code 200
   end
 end

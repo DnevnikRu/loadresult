d7429185f5b95f1b46b3cca99696a4b0ee81742f
Evgeny Morozov <evmorov@gmail.com>
2016-03-30 17:42:19 +0300

    DA-631 Add couple of tests to Show Result page

---
+++ spec/features/show_result_spec.rb
@@ -0,0 +1,22 @@
+require 'feature_helper'
+
+describe 'Show Result page' do
+  before do
+    @result = create(:result)
+  end
+
+  scenario 'Show Result page can be open from the results page' do
+    visit '/results/'
+    within(:xpath, "//td[@class='id' and text()='#{@result.id}']/..") do
+      find('.showResult').click
+    end
+
+    expect(page).to have_current_path(result_path(@result))
+  end
+
+  scenario 'Show Result page shows id of a result' do
+    visit result_path(@result)
+
+    expect(page).to have_content(@result.id)
+  end
+end

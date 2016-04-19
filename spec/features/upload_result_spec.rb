eb4a6db82120be79c045461d641f389fb433fa8e
Evgeny Morozov <evmorov@gmail.com>
2016-04-19 15:43:26 +0300

    Remove Upload result button from Results page

--- spec/features/upload_result_spec.rb
+++ spec/features/upload_result_spec.rb
@@ -1,15 +1,6 @@
 require 'feature_helper'
 
 feature 'Upload load result' do
-  scenario 'Open the upload page from the homepage' do
-    visit '/'
-    within('.container') do
-      click_link 'Upload results'
-    end
-
-    expect(page).to have_css('h1', text: 'New Result')
-  end
-
   scenario 'Open the upload page using the navigation bar' do
     visit '/'
     within('.navbar') do

c87ab946e6b3f363577b5296db98d65400f5dafa
Evgeny Morozov <evmorov@gmail.com>
2016-04-19 13:25:12 +0300

    DA-700 Результатов может быть выбрано только 2

--- app/controllers/trend_controller.rb
+++ app/controllers/trend_controller.rb
@@ -1,14 +1,18 @@
 class TrendController < ApplicationController
   def show
-    if params[:result].nil? || params[:result].size == 1
-      return redirect_to results_path, alert: 'You should select 2 or more results to create a trend'
+    if params[:result].nil? || params[:result].size != 2
+      return redirect_to results_path, alert: 'You should select 2 results to create a trend'
     end
 
     results = params[:result].map do |result_id|
       Result.find_by(id: result_id)
     end
+
     return redirect_to(results_path, alert: "Can't find selected results") if results.include? nil
-    return redirect_to(results_path, alert: "Can't create a trend for 2 results") if results.size == 2
+    if results.size == 2
+      message = "There are only 2 results between selected results. Can't create a trend"
+      return redirect_to(results_path, alert: message)
+    end
 
     flash[:result_ids] = nil # reset choosen results on the result index page
   end

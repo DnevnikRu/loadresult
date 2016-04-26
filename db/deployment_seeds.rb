af9764ae3fd6bd7920faf85ead18d64c1a30873c
Evgeny Morozov <evmorov@gmail.com>
2016-04-26 18:36:50 +0300

    Update seeds

--- db/deployment_seeds.rb
+++ db/deployment_seeds.rb
@@ -14,6 +14,7 @@ results = []
                            profile: 'all_sites',
                            time_cutting_percent: 10,
                            test_run_date: "0#{i+1}.02.2016 10:0#{i}",
+                           release_date: "0#{i+1}.02.2016 10:0#{i}",
                            created_at: "0#{i+1}.02.2016 10:0#{i}")
 end
 
@@ -25,19 +26,19 @@ results.each do |result|
                             value: (0 + random.rand(1000)),
                             response_code: [200, 500, 200, 200].sample,
                             result_id: result.id)
-
-      CalculatedRequestsResult.create(label: label,
-                                      mean: (150 + random.rand(100)),
-                                      median: (150 + random.rand(100)),
-                                      ninety_percentile: (150 + random.rand(100)),
-                                      max: (200 + random.rand(10)),
-                                      min: (0 + random.rand(10)),
-                                      failed_results: (0 + random.rand(10)),
-                                      result_id: result.id
-      )
-
     end
+    CalculatedRequestsResult.create(label: label,
+                                    mean: (150 + random.rand(100)),
+                                    median: (150 + random.rand(100)),
+                                    ninety_percentile: (150 + random.rand(100)),
+                                    max: (200 + random.rand(10)),
+                                    min: (0 + random.rand(10)),
+                                    failed_results: (0 + random.rand(10)),
+                                    result_id: result.id,
+                                    throughput: 0.1
+    )
   end
+
   ['web00 EXEC Network\Bytes Sent/sec', 'web11 EXEC Network\Bytes Sent/sec', 'web11 CPU Processor Time',
    'web00 CPU Processor Time', 'web00 Memory Memory\Available', 'web11 Memory Memory\Available'].each do |label|
     10.times do |i|
@@ -46,12 +47,12 @@ results.each do |result|
                                value: (0 + random.rand(1000)),
                                result_id: result.id)
 
-      CalculatedPerformanceResult.create(label: label,
-                                         mean: (150 + random.rand(100)),
-                                         max: (200 + random.rand(10)),
-                                         min: (0 + random.rand(10)),
-                                         result_id: result.id
-      )
     end
+    CalculatedPerformanceResult.create(label: label,
+                                       mean: (150 + random.rand(100)),
+                                       max: (200 + random.rand(10)),
+                                       min: (0 + random.rand(10)),
+                                       result_id: result.id
+    )
   end
 end

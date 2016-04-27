b47fa54becbad2e39129fa7a61ee9f7235e9bc3a
Evgeny Morozov <evmorov@gmail.com>
2016-04-27 13:04:06 +0300

    Better seeds

--- db/deployment_seeds.rb
+++ db/deployment_seeds.rb
@@ -6,21 +6,22 @@ CalculatedPerformanceResult.delete_all
 
 random = Random.new(123)
 results = []
-10.times do |i|
+25.times do |i|
+  result_date = "#{random.rand(1..28)}.#{random.rand(1..12)}.2016 10:00"
   results << Result.create(project_id: Project.find_by(project_name: 'Dnevnik').id,
                            version: "master-#{i+1}",
                            duration: 20,
                            rps: 100,
                            profile: 'all_sites',
                            time_cutting_percent: random.rand(10..11),
-                           test_run_date: "#{random.rand(1..28)}.#{random.rand(1..12)}.2016 10:00",
-                           release_date: "#{random.rand(1..28)}.#{random.rand(1..12)}.2016 10:00",
-                           created_at: "#{random.rand(1..28)}.#{random.rand(1..12)}.2016 10:00")
+                           test_run_date: result_date,
+                           release_date: result_date,
+                           created_at: result_date)
 end
 
 results.each do |result|
   ['login :GET', 'root /counters:GET', 'schools /ajax.ashx:POST:a=jme', 'root /user/:GET'].each do |label|
-    10.times do |i|
+    20.times do |i|
       RequestsResult.create(timestamp: 1453280230000 + (i+1)*1000,
                             label: label,
                             value: (0 + random.rand(1000)),
@@ -41,7 +42,7 @@ results.each do |result|
 
   ['web00 EXEC Network\Bytes Sent/sec', 'web11 EXEC Network\Bytes Sent/sec', 'web11 CPU Processor Time',
    'web00 CPU Processor Time', 'web00 Memory Memory\Available', 'web11 Memory Memory\Available'].each do |label|
-    10.times do |i|
+    20.times do |i|
       PerformanceResult.create(timestamp: 1453280230000 + (i+1)*1000,
                                label: label,
                                value: (0 + random.rand(1000)),

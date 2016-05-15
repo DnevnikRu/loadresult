c73353295f729da5e55ab55880f4e7050d7927b7
Moskvin Aleksey <mskvn91@gmail.com>
2016-05-15 18:58:28 +0300

    DA-709 start work on performance graphs in trend report

--- app/models/trend_report.rb
+++ app/models/trend_report.rb
@@ -22,8 +22,8 @@ class TrendReport
 
     result_from, result_to = [result1, result2].sort_by(&:release_date)
     results_between = Result.where(
-        release_date: (result_from.release_date..result_to.release_date),
-        project_id: result1.project_id
+      release_date: (result_from.release_date..result_to.release_date),
+      project_id: result1.project_id
     )
     if results_between.size == 2
       @error = "There are only 2 results between selected results. Can't create a trend"
@@ -37,18 +37,18 @@ class TrendReport
     diff = []
     results.each do |result|
       if diff.empty? ||
-          diff.last[:duration] != result.duration ||
-          diff.last[:rps] != result.rps ||
-          diff.last[:profile] != result.profile ||
-          diff.last[:time_cutting_percent] != result.time_cutting_percent ||
-          diff.last[:value_smoothing_interval] != result.value_smoothing_interval
+        diff.last[:duration] != result.duration ||
+        diff.last[:rps] != result.rps ||
+        diff.last[:profile] != result.profile ||
+        diff.last[:time_cutting_percent] != result.time_cutting_percent ||
+        diff.last[:value_smoothing_interval] != result.value_smoothing_interval
         diff.push(
-            ids: [result.id],
-            duration: result.duration,
-            rps: result.rps,
-            profile: result.profile,
-            time_cutting_percent: result.time_cutting_percent,
-            value_smoothing_interval: result.value_smoothing_interval
+          ids: [result.id],
+          duration: result.duration,
+          rps: result.rps,
+          profile: result.profile,
+          time_cutting_percent: result.time_cutting_percent,
+          value_smoothing_interval: result.value_smoothing_interval
         )
       else
         diff.last[:ids].push result.id
@@ -63,6 +63,9 @@ class TrendReport
       [label, percent]
     end.sort_by { |arr| arr[1] }.reverse
   end
+
+  def performance_groups
+      end
 
   def request_labels
     results.inject([]) do |labels, result|

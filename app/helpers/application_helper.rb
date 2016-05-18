ae11917c192aeb1decd58880f9878d809ef822fe
Евгений Морозов <morozov@company.dnevnik.ru>
2016-05-18 10:03:32 +0300

    DA-696 Show comment on hover on icon

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
   def parameter_to_compare?(parameter)
     ['Rps', 'Duration', 'Profile', 'Time cutting percent', 'Value smoothing interval'].include? parameter
   end
+
+  def comment_icon(comment)
+    "<i class=\"glyphicon glyphicon-envelope data-toggle=\"tooltip\" title=\"#{comment}\"></i>".html_safe
+  end
 end

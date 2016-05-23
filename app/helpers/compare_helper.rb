153bc3ed054d73f1455bbaf3014608f428a26c67
Евгений Морозов <morozov@company.dnevnik.ru>
2016-05-23 16:06:31 +0300

    DA-668 Add anchors for Report result page

--- app/helpers/compare_helper.rb
+++ app/helpers/compare_helper.rb
@@ -7,8 +7,14 @@ module CompareHelper
     link_to "#{prefix}#{result_id}", result_path(result_id)
   end
 
-  def link_to_anchor(compare_report, anchor_id)
-    link_to compare_path(result: [compare_report.result1.id, compare_report.result2.id], anchor: anchor_id), {class: 'small link-unstyled'} do
+  def link_to_anchor_on_compare(anchor_id, results_ids)
+    link_to compare_path(result: results_ids, anchor: anchor_id), {class: 'small link-unstyled'} do
+      '<i class="glyphicon glyphicon-link"></i>'.html_safe
+    end
+  end
+
+  def link_to_anchor_on_report(anchor_id, id)
+    link_to report_result_path(result: id, anchor: anchor_id), {class: 'small link-unstyled'} do
       '<i class="glyphicon glyphicon-link"></i>'.html_safe
     end
   end

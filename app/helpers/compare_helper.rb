cf85bec3f520fb83d4a7cd995b4742784720d546
Евгений Морозов <morozov@company.dnevnik.ru>
2016-05-23 15:04:27 +0300

    DA-668 Add anchors for perforamnce results

--- app/helpers/compare_helper.rb
+++ app/helpers/compare_helper.rb
@@ -6,4 +6,14 @@ module CompareHelper
   def link_to_result(result_id, prefix = nil)
     link_to "#{prefix}#{result_id}", result_path(result_id)
   end
+
+  def link_to_anchor(compare_report, anchor_id)
+    link_to compare_path(result: [compare_report.result1.id, compare_report.result2.id], anchor: anchor_id), {class: 'small link-unstyled'} do
+      '<i class="glyphicon glyphicon-link"></i>'.html_safe
+    end
+  end
+
+  def anchor(anchor_id)
+    content_tag 'span', nil, {id: anchor_id, class: 'request-bookmark'}
+  end
 end

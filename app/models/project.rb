75187765be0f90bfc1dac8260a40a5efdf71d176
Martha Subbota <marta.subbota@mbp-marta.it.lan>
2017-05-30 17:03:05 +0300

    project add, edit, delete

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,7 +1,26 @@
 class Project < ActiveRecord::Base
-  has_many :results
+  has_many :results, dependent: :delete_all
+
+  validates :project_name, presence: true
 
   def self.options_for_select
-    order('LOWER(project_name)').map { |e| [e.project_name, e.id] }
+    order('LOWER(project_name)').map {|e| [e.project_name, e.id]}
   end
+
+  def self.create_new_project(params)
+    project = Project.new(
+        project_name: params['project_name']
+    )
+    project.save
+    project
+  end
+
+  def self.edit_project(project, params)
+    project.update(
+        project_name: params[:project_name]
+    )
+    project
+  end
+
+
 end

05c8af5cbce4b8f6250999cf48913914c75a95f4
Aleksei Moskvin <a.moskvin@propellerads.net>
2018-07-12 21:40:00 +0300

    change migration

--- db/migrate/20180110132246_calculate_last_value.rb
+++ db/migrate/20180110132246_calculate_last_value.rb
@@ -1,6 +1,6 @@
 class CalculateLastValue < ActiveRecord::Migration
   def change
-    results = Result.where('test_run_date >= ?', DateTime.parse('2017-12-15'))
+    results = Result.where('test_run_date >= ?', DateTime.parse('2018-07-12'))
     results.each do |result|
       Result.calculate_last_value(result)
     end

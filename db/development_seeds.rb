a8e28f20b1985cee209c7c1f9b4cfbab3ae5b6b1
Aleksei Moskvin <a.moskvin@propellerads.net>
2018-07-12 21:55:34 +0300

    moved all seeds to develop seeds

--- db/development_seeds.rb
+++ db/development_seeds.rb
@@ -1,3 +1,121 @@
+PerformanceGroup.destroy_all
+PerformanceLabel.destroy_all
+Project.destroy_all
+groups = [{
+              name: 'Processor',
+              units: '%',
+              trend_limit: 0,
+              labels: [PerformanceLabel.create(label: 'CPU Processor Time'),
+                       PerformanceLabel.create(label: 'CPU')]
+          },
+          {
+              name: 'Requests per second',
+              units: 'req/sec',
+              trend_limit: 1,
+              labels: [PerformanceLabel.create(label: 'EXEC ASP.NET Apps(_total_)\Req/sec')]
+          },
+          {
+              name: 'Requests executing',
+              units: 'requests',
+              trend_limit: 0.1,
+              labels: [PerformanceLabel.create(label: 'EXEC ASP.NET Apps(_total_)\Requests Executing')]
+          },
+          {
+              name: 'Requests Current',
+              units: 'requests',
+              trend_limit: 0.1,
+              labels: [PerformanceLabel.create(label: 'EXEC ASP.NET\Requests Current')]
+          },
+          {
+              name: 'DB Server Connections',
+              units: 'users',
+              trend_limit: 1,
+              labels: [PerformanceLabel.create(label: 'EXEC SQLServer User connections')]
+          },
+          {
+              name: 'Memory Available',
+              units: 'Mb',
+              trend_limit: 10,
+              labels: [PerformanceLabel.create(label: 'Memory Memory\Available')]
+          },
+          {
+              name: 'Memory Page Faults',
+              units: 'page/sec',
+              trend_limit: 10,
+              labels: [PerformanceLabel.create(label: 'Memory\Page Faults/sec')]
+          },
+          {
+              name: 'Memory Page',
+              units: 'page/sec',
+              trend_limit: 1,
+              labels: [PerformanceLabel.create(label: 'Memory\Page/sec')]
+          },
+          {
+              name: 'Network traffic',
+              units: 'Mb/sec',
+              trend_limit: 100,
+              labels: [PerformanceLabel.create(label: 'Network\Bytes Sent/sec'),
+                       PerformanceLabel.create(label: 'Network\Bytes Received/sec'),
+                       PerformanceLabel.create(label: 'Network I/O')]
+          },
+          {
+              name: 'Network queue',
+              units: 'unit',
+              trend_limit: 0.01,
+              labels: [PerformanceLabel.create(label: 'Network\Output Queue Length')]
+          },
+          {
+              name: 'Disk queue',
+              units: 'unit',
+              trend_limit: 0.5,
+              labels: [PerformanceLabel.create(label: 'Disk(inst_1)\Avg. Write Queue'),
+                       PerformanceLabel.create(label: 'Disk(inst_1)\Avg. Read Queue'),
+                       PerformanceLabel.create(label: 'Disks I/O')]
+          },
+          {
+              name: 'Error per second',
+              units: 'error/sec',
+              trend_limit: 0.01,
+              labels: [PerformanceLabel.create(label: 'ASP.NET Apps(_total_)\Errors Total/Sec')]
+          },
+          {
+              name: 'Requests in application queue',
+              units: 'requests',
+              trend_limit: 0.01,
+              labels: [PerformanceLabel.create(label: 'ASP.NET Apps(_total_)\Requests In Application Queue')]
+          },
+          {
+              name: 'DB Server Cache',
+              units: '%',
+              trend_limit: 0,
+              labels: [PerformanceLabel.create(label: 'SQLServer Buffer cache hit ratio'),
+                       PerformanceLabel.create(label: 'SQLServer Plane cache hit ratio')]
+          },
+          {
+              name: 'DB Server Waits',
+              units: 'ms',
+              trend_limit: 0.01,
+              labels: [PerformanceLabel.create(label: 'SQLServer Lock Avg Wait Time')]
+          },
+          {
+              name: 'DB Server Locks',
+              units: 'locks',
+              trend_limit: 0.01,
+              labels: [PerformanceLabel.create(label: 'SQLServer Lock Timeouts/sec')]
+          },
+          {
+              name: 'DB Server Statistics',
+              units: 'unit',
+              trend_limit: 1,
+              labels: [PerformanceLabel.create(label: 'SQLServer Errors/sec'),
+                       PerformanceLabel.create(label: 'SQLServer Re-Compilations/sec'),
+                       PerformanceLabel.create(label: 'SQLServer Number of Deadlocks')]
+          } ]
+groups.each { |group| PerformanceGroup.create(group) }
+
+Project.create(id: 1, project_name: 'Dnevnik')
+Project.create(id: 2, project_name: 'Contingent')
+
 Result.destroy_all
 RequestsResult.delete_all
 CalculatedRequestsResult.delete_all
